3ab06f024312086489600fc9781cfe247ecfc507
woodmawa <will.woodman@btinternet.com>
2020-11-06 16:54:57 +0000

    dynamic table building

--- src/main/groovy/scripts/dynamicTableFromColumns.groovy
+++ src/main/groovy/scripts/dynamicTableFromColumns.groovy
@@ -11,10 +11,20 @@ Column ic2 = IntColumn.create ("Integer column2", 1, 2)
 //seems to defined by columns at point of adding, appending to column doesnt change the dataframe 2x2
 Table df = Table.create(sc1, ic2)
 
-ic2.append(3)
-println ic2.print()
-
-
 println df.shape()
 println df.structure()
 println df.printAll()
+
+//cant create
+ic2.append(3)
+println ic2.print()
+
+//add empty cell ref to column
+sc1.appendMissing()
+
+//this is ok as columns still have same length
+Table df2 = Table.create(sc1, ic2)
+println df2.shape()
+println df2.structure()
+println df2.printAll()
+
